0d1fda17a14874aa72b712ca0faceb417ddd2238
Pliavi <pliavi.rpgm@gmail.com>
2020-10-19 19:34:48 -0300

    removed all old files

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/css/reset.css';
-import './assets/css/global.css';
-import "leaflet/dist/leaflet.css"
-import router from "./router"
-
-createApp(App).use(router).mount('#app')
