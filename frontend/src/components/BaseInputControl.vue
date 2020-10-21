d64a8509e484889e7d9a2a1c07f87c51c9b6dbcf
Pliavi <pliavi.rpgm@gmail.com>
2020-10-21 14:58:06 -0300

    create base input control

---
+++ frontend/src/components/BaseInputControl.vue
@@ -0,0 +1,35 @@
+<template>
+  <label>
+    <div class="mt-6 mb-2 text-color-title">
+      <span class="mr-8 font-semibold">{{ label }}</span>
+      <span v-if="instructions" class="text-sm">{{ instructions }}</span>
+    </div>
+    <div class="input-wrapper">
+      <slot></slot>
+    </div>
+  </label>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class BaseInputControl extends Vue {
+  @Prop({ type: String, required: true }) label: string | undefined;
+  @Prop(String) instructions: string | undefined;
+}
+</script>
+
+<style lang="postcss" scoped>
+.input-wrapper >>> input,
+.input-wrapper >>> textarea {
+  @apply border border-gray-200 rounded-2xl bg-gray-50 w-full p-4 text-gray-500;
+}
+
+.input-wrapper >>> input {
+  @apply h-16;
+}
+
+.input-wrapper >>> textarea {
+  @apply h-24;
+}
+</style>
